028e10c53c15d941bfd3dafe2186c75453cbad40
Coleman Jackson <burrard.bc@gmail.com>
2018-02-28 20:28:11 -0800

    who knows

--- server.js
+++ server.js
@@ -1,25 +1,42 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
-// const yahooUrl = 'https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20%3D%202487889&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
-//
+const yahooUrl = 'https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20%3D%202487889&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
+
 // app.get('/api/yahoo', (req, res) => {
 //   axios.get(yahooUrl).then(({data}) => {
 //     res.send(data);
 //   });
 // });
 
-axios.get('/api/yahoo', {
-  method:'get',
-  url:'http://bit.ly/2mTM3nY',
-  responseType:'stream'
-})
-  .then(function(response) {
-    response.data.pipe(fs.createWriteStream('ada_lovelace.jpg'))
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+// axios.get('/api/yahoo', {
+//   method:'get',
+//   url:'http://bit.ly/2mTM3nY',
+//   responseType:'stream'
+// })
+//   .then(function(response) {
+//     response.data.pipe(fs.createWriteStream('ada_lovelace.jpg'))
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+
+// axios.get('/api/yahoo')
+//   .then(function (response) {
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+//
+// axios({
+//   method:'get',
+//   url:'http://bit.ly/2mTM3nY',
+//   responseType:'stream'
+// })
+//   .then(function(response) {
+//     response.data.pipe(fs.createWriteStream('ada_lovelace.jpg'))
+//   });
 
 app.use(express.static('public'));
 
