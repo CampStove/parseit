6d519fddf87ca13824ece9061c1024a417b08980
Coleman Jackson <burrard.bc@gmail.com>
2018-03-01 15:42:21 -0800

    basic get request

--- server.js
+++ server.js
@@ -3,11 +3,17 @@ const app = express();
 const axios = require('axios');
 const yahooUrl = 'https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20%3D%202487889&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
 
-app.get('/api/yahoo', (req, res) => {
-  axios.get(yahooUrl).then(({data}) => {
-    res.send(data);
+axios.get('https://api.github.com/users/' + username)
+  .then(function(response){
+    console.log(response.data); // ex.: { user: 'Your User'}
+    console.log(response.status); // ex.: 200
   });
-});
+
+// app.get('/api/yahoo', (req, res) => {
+//   axios.get(yahooUrl).then(({data}) => {
+//     res.send(data);
+//   });
+// });
 
 app.use(express.static('public'));
 app.listen(8080, () => console.log('listening on port 8080'));
