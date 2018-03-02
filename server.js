3944bb8fb4c2301b9595256c088cdb0dd4c0c053
Coleman Jackson <burrard.bc@gmail.com>
2018-03-01 23:06:35 -0800

    axios for tom

--- server.js
+++ server.js
@@ -9,11 +9,34 @@ const yahooUrl = 'https://query.yahooapis.com/v1/public/yql?q=select%20item.cond
 //     console.log(response.status); // ex.: 200
 //   });
 
-// app.get('/api/yahoo', (req, res) => {
-//   axios.get(yahooUrl).then(({data}) => {
-//     res.send(data);
+app.get('/api/yahoo', (req, res) => {
+  axios.get(yahooUrl).then(({data}) => {
+    res.send(data);
+  });
+});
+
+// const request = require('request');
+// app.get('/api/yahoo', function(req, res) {
+//   request(yahooUrl, function(error, response, body) {
+//     res.JSON.parse({body})
 //   });
 // });
+
+// const getData = async function getData() {
+//   const { payload } = await wreck.get(conditions);
+//   const paths = JSON.parse(payload);
+//
+//   const output = reply
+//     .response({ paths })
+//     .type('application/json');
+//   resolve(output.source.paths.query.results.channel.item.condition);
+// };
+//
+// try {
+//   getData();
+// } catch (error) {
+//   reject(error);
+// }
 
 app.use(express.static('public'));
 app.listen(8080, () => console.log('listening on port 8080'));
