887ab74ae72ac0ac6b1a090ff4dd2897374ac71d
Coleman Jackson <burrard.bc@gmail.com>
2018-03-01 16:07:37 -0800

    username for git *testing*

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const axios = require('axios');
 const yahooUrl = 'https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20%3D%202487889&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
 
-axios.get('https://api.github.com/users/' + username)
+axios.get('https://api.github.com/users/' + CampStove)
   .then(function(response){
     console.log(response.data); // ex.: { user: 'Your User'}
     console.log(response.status); // ex.: 200
