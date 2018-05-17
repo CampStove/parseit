a4b36a72ebc608d1c2827b2cfa11992686a7ec71
Coleman Jackson <burrard.bc@gmail.com>
2018-05-17 16:51:00 -0700

    resume.js

--- resume.js
+++ resume.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.use(express.static('resume'));
+app.use(express.static('resume.txt'));
 app.listen(8081, () => console.log('listening on port 8081'));
