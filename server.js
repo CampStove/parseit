a93268707a93fc78d525cc02990ae3d6ff9327b1
Coleman Jackson <burrard.bc@gmail.com>
2018-05-16 19:34:58 -0700

    no api try again

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-//const axios = require('axios');
+const axios = require('axios');
 
 // const yahooUrl = 'https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20%3D%209807&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
 // app.get('/api/yahoo', (req, res) => {
