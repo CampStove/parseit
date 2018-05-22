9e8938f92d105ed8b2908e3d3200ddca6f24da7e
Coleman Jackson <burrard.bc@gmail.com>
2018-05-21 17:52:26 -0700

    bss

--- public/js/index.js
+++ public/js/index.js
@@ -30,3 +30,40 @@ document.getElementById("date").innerText = nowFormat;
 //   const url = '/api/ipapi';
 // }
 // ipApi();
+
+// slider
+var galleryArray = [
+  "ambleside_park_20170528_044612_CJI__71U3569",
+  "wreck_beach_20170527_213210_CJI__71U3539",
+  "lynn_canyon_20170531_205334_CJI__71U3787",
+  "ambleside_park_20170528_053453_CJI__71U3662",
+  "ubc_20170528_205127_CJI__71U3700",
+  "ambleside_park_20170528_053818_CJI__71U3665",
+  "lynn_canyon_20170531_205146_CJI__71U3781",
+  "ubc_20170528_210444_CJI__71U3736",
+  "wreck_beach_20170527_210656_CJI__71U3524",
+  "ubc_20170601_213403_CJI__71U3824"
+];
+const slider = document.getElementById('slider');
+let index = 0;
+function sliderFunc(controlBtn){
+  console.log(controlBtn);
+  galleryArray.sort()
+  if(controlBtn === "next"){
+    if(index === (galleryArray.length - 1)){
+      index = 0;
+    }
+    else{
+      index += 1;
+    }
+  }
+  else if(controlBtn === "prev"){
+    if(index === 0){
+      index = (galleryArray.length - 1);
+    }
+    else {
+      index -= 1;
+    }
+  }
+  slider.src = "img/" + galleryArray[index] + ".jpg";
+}
